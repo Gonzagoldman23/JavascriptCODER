82b5589c83a9854ef3bc56b716bfc5d268d69b87
Gonzagoldman23 <gonzagoldman23@gmail.com>
2023-07-19 23:12:11 -0300

    Entrega final

---
+++ libraries.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('send');
+const form = document.getElementById('form');
+
+button.addEventListener('click', () => {
+    swal({
+        title: 'Perfecto!',
+        text: 'Tu mensaje fue enviado',
+        icon: 'success',
+        confirmButtonText: ''
+    })
+})
